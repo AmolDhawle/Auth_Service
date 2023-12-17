d4d324a3da931a8e310ffef6a367e5ec4a5861d1
Amol Dhawle <amoldhawle2@gmail.com>
2023-12-17 20:28:28 +0530

    minor changes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ const { PORT } = require('./config/serverConfig');
 const app = express();
 const apiRoutes = require('./routes/index');
 const db = require('./models/index');
-const { User, Role } = require('./models/index');
 
 // const UserService  = require('./services/user-service');
 
@@ -19,13 +18,6 @@ const prepareAndStartServer = () => {
         if(process.env.DB_SYNC) {
             db.sequelize.sync({ alter: true });
         }
-
-        const u1 = await User.findByPk(4);
-        const r1 = await Role.findByPk(2);
-        // u1.addRole(r1);
-
-        const response = await u1.getRoles();
-        console.log(response);
 
         // const service = new UserService();
         // const newToken = service.createToken({email: 'ganesh@admin.com', id: 1});
